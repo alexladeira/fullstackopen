b48c5b7feddb8d20a50d9d6efaa332f9680c19c2
Alexandre Ladeira <alexandre.ladeira@gmail.com>
2019-09-09 15:54:00 -0300

    fullstackopen.com - exercise part 1d.anecdotes - step 2

--- part1.d.anecdotes/src/App.js
+++ part1.d.anecdotes/src/App.js
@@ -2,14 +2,6 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [selected, setSelected] = useState(0);
-
-  const next = () => {
-    setSelected(
-      anecdotes[Math.floor(Math.random() * Math.floor(anecdotes.length))]
-    );
-  };
-
   const anecdotes = [
     "If it hurts, do it more often",
     "Adding manpower to a late software project makes it later!",
@@ -18,10 +10,30 @@ function App() {
     "Premature optimization is the root of all evil.",
     "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it."
   ];
+  const [selected, setSelected] = useState(0);
+  const [votes, updateVotes] = useState(
+    Array.apply(null, new Array(anecdotes.length)).map(
+      Number.prototype.valueOf,
+      0
+    )
+  );
+
+  const next = () =>
+    setSelected(Math.floor(Math.random() * Math.floor(anecdotes.length)));
+
+  const vote = () => {
+    let v = [...votes];
+    v[selected] += 1;
+    updateVotes(v);
+  };
 
   return (
     <div className="App">
-      <p>{selected}</p>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button type="button" onClick={vote}>
+        vote
+      </button>
       <button type="button" onClick={next}>
         next anecdote
       </button>
