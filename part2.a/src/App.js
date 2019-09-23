f01037183b5462e57c4bf3bbec141ec44e424856
Alexandre Ladeira <alexandre.ladeira@gmail.com>
2019-09-23 15:32:32 -0300

    fullstackopen.com - part 2 - step 9

--- part2.a/src/App.js
+++ part2.a/src/App.js
@@ -4,27 +4,50 @@ import "./components/course/Course";
 import Course from "./components/course/Course";
 
 function App() {
-  const course = {
-    title: "Half Stack application development",
-    parts: [
-      {
-        title: "Fundamentals of React",
-        exercises: 10,
-        id: 0
-      },
-      {
-        title: "Using props to pass data",
-        exercises: 7,
-        id: 1
-      },
-      {
-        title: "State of a component",
-        exercises: 14,
-        id: 2
-      }
-    ]
-  };
-  return <Course course={course} />;
+  const courses = [
+    {
+      title: "Half Stack application development",
+      id: 0,
+      parts: [
+        {
+          title: "Fundamentals of React",
+          exercises: 10,
+          id: 0
+        },
+        {
+          title: "Using props to pass data",
+          exercises: 7,
+          id: 1
+        },
+        {
+          title: "State of a component",
+          exercises: 14,
+          id: 2
+        }
+      ]
+    },
+    {
+      title: "Node.js",
+      id: 1,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 0
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 1
+        }
+      ]
+    }
+  ];
+  return courses.map(course => (
+    <>
+      <Course course={course} key={course.id} />
+    </>
+  ));
 }
 
 export default App;
