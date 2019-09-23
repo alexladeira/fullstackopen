e570e19437aaa4995512a187619c85d6766e79d2
Alexandre Ladeira <alexandre.ladeira@gmail.com>
2019-09-23 16:42:23 -0300

    fullstackopen.com - part2.b - phonebook step 1

---
+++ part2.b/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPhone = event => {
+    event.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName("");
+  };
+
+  const handleNewName = event => setNewName(event.target.value);
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPhone}>
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Entries persons={persons} />
+    </div>
+  );
+};
+
+const Entries = ({ persons }) =>
+  persons.map(person => <Entry name={person.name} key={person.name} />);
+
+const Entry = ({ name }) => (
+  <div>
+    <span> {name} </span>
+  </div>
+);
+
+export default App;
