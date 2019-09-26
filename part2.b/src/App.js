db1ea4122002edbac99f8c1395424bf89106ce57
Alexandre Ladeira <alexandre.ladeira@gmail.com>
2019-09-26 15:57:29 -0300

    fullstackopen.com - part2.b - phonebook step 4

--- part2.b/src/App.js
+++ part2.b/src/App.js
@@ -3,23 +3,34 @@ import "./App.css";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phoneNumber: "5521999999999" }
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" }
   ]);
   const [newName, setNewName] = useState("");
   const [newPhoneNumber, setNewPhoneNumber] = useState("");
+  const [filter, setNewFilter] = useState("");
 
   const handleNewName = event => setNewName(event.target.value);
 
   const handleNewPhoneNumber = event => setNewPhoneNumber(event.target.value);
+
+  const handleFilter = event => setNewFilter(event.target.value);
+
+  const displayPersons = () =>
+    filter === ""
+      ? persons
+      : persons.filter(
+          el => el.name.toLowerCase().indexOf(filter.toLowerCase()) >= 0
+        );
 
   const addPhone = event => {
     event.preventDefault();
     if (persons.filter(el => el.name === newName).length > 0) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons(
-        persons.concat({ name: newName, phoneNumber: newPhoneNumber })
-      );
+      setPersons(persons.concat({ name: newName, number: newPhoneNumber }));
       setNewName("");
       setNewPhoneNumber("");
     }
@@ -28,6 +39,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <span>
+        filter <input type="text" onChange={handleFilter} />
+      </span>
+      <h3>Add new contact</h3>
       <form onSubmit={addPhone}>
         <div>
           name: <input value={newName} onChange={handleNewName} />
@@ -41,7 +56,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Entries persons={persons} />
+      <Entries persons={displayPersons()} />
     </div>
   );
 };
@@ -52,7 +67,7 @@ const Entries = ({ persons }) =>
 const Entry = ({ person }) => (
   <div>
     <span>
-      {person.name} - {person.phoneNumber}
+      {person.name} - {person.number}
     </span>
   </div>
 );
