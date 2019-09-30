33103b06900a8989e365328432bd8ba2a99b5a06
Alexandre Ladeira <alexandre.ladeira@gmail.com>
2019-09-30 10:43:47 -0300

    fullstackopen.com - part2.c - countries data - step 1

--- part2.c.countries/src/App.js
+++ part2.c.countries/src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const Language = ({ lang }) => <li>{lang}</li>;
+
+const Languages = ({ languages }) =>
+  languages.map(el => <Language lang={el.name} key={el.name} />);
+
+const Country = ({ country, nameOnly }) =>
+  nameOnly ? (
+    <p>{country.name}</p>
+  ) : (
+    <>
+      <h2>{country.name}</h2>
+      <p>capital: {country.capital}</p>
+      <p>population: {country.population}</p>
+      <h3>languages</h3>
+      <ul>
+        <Languages languages={country.languages} />
+      </ul>
+      <img src={country.flag} alt={country.name} height="180" width="180" />
+    </>
   );
-}
+
+const Countries = ({ countries }) =>
+  countries.length > 10 ? (
+    <p> Too many results </p>
+  ) : countries.length > 1 && countries.length < 10 ? (
+    countries.map(el => (
+      <Country country={el} nameOnly="true" key={el.alpha2Code} />
+    ))
+  ) : countries.length === 1 ? (
+    <Country country={countries[0]} />
+  ) : (
+    []
+  );
+
+const App = () => {
+  const [filter, setFilter] = useState("");
+  const [countries, setCountries] = useState([]);
+
+  const handleChange = event => setFilter(event.target.value);
+
+  useEffect(() => {
+    if (filter && filter.length > 0) {
+      const fetchData = async () => {
+        const result = await axios.get(
+          `https://restcountries.eu/rest/v2/name/${filter}`
+        );
+        setCountries(result.data);
+      };
+      fetchData();
+    }
+  }, [filter]);
+
+  return (
+    <>
+      <p>
+        find countries <input type="text" onChange={handleChange} />
+      </p>
+      <Countries countries={countries} />
+    </>
+  );
+};
 
 export default App;
